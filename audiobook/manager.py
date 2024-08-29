cf72b73c04ef781b7ef8534a441c0505cc36cdd0
Edcarloszaya <edcarlos.molequedoido@gmail.com>
2024-08-29 19:18:15 -0300

    A funcao extrair_texto: abrir ler o arquivo pdf e extrai o texto do pdf e o retorna

--- audiobook/manager.py
+++ audiobook/manager.py
@@ -1,7 +1,10 @@
 import tkinter as tk
 from tkinter import filedialog
+from gtts import gTTS
+import fitz
 
-class AudioBook():
+
+class AudioBook:
 
     def __init__(self) -> None:
         pass
@@ -11,10 +14,19 @@ class AudioBook():
         janela = tk.Tk()
         janela.withdraw()
 
-        caminho_do_arquivo = filedialog.askopenfilename(title="Selecione o arquivo PDF",
-                                                        filetypes=[("Arquivos PDF","*.pdf")])
+        caminho_do_arquivo = filedialog.askopenfilename(
+            title="Selecione o arquivo PDF", filetypes=[("Arquivos PDF", "*.pdf")]
+        )
         return caminho_do_arquivo
-    
-    
+
+    def extrair_texto(self, caminho_do_PDF):
+        documento = fitz.open(caminho_do_PDF)
+        texto = ""
+
+        for page_num in range(len(documento)):
+            pagina = documento.load_page(page_num)
+            texto += pagina.get_text()
+
+        return texto
 
     
