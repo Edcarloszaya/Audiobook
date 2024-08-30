ec50045826b9e36a1201862eb35df1c8e1364448
Edcarloszaya <edcarlos.molequedoido@gmail.com>
2024-08-30 10:22:48 -0300

    Intancia a classe AudioBook,cria duas funcoes menu e main:
    menu : mostra logo e msg de bem vindo
    
    main : funcao principal onde organiza cada passo pra comversao do pdf pra audio

---
+++ audiobook/main.py
@@ -0,0 +1,50 @@
+from manager import AudioBook
+from logo import logo
+
+
+# instacia classe
+audio = AudioBook()
+
+def menu():
+    audio.limpar_tela()
+    print(logo)
+    print("\nBem vindo ao AudioBook\n")
+
+def main():
+
+    execucao = True
+
+    while execucao:
+        
+    #   menu
+        menu()
+        escolha = input(f"Para seleciona PDF tecla : [1] ? \nPara sair tecla: [x] ?")
+
+        if escolha == '1':
+            # busca pdf
+            menu()
+            PDF = audio.selecionar_arquivo()
+
+            # extrai texto 
+            menu()
+            texto = audio.extrair_texto(PDF)
+            
+            # comverti pra audio
+            audio.comverti_texto(texto)
+
+            criar_novamemte = input('\nDesejar criar outro Audiobook: s=[Sim] ou n=[Nao] ?')
+            if criar_novamemte == 's':
+                execucao = False
+                main()
+                        
+            else:
+                execucao = False
+                audio.limpar_tela()
+
+
+        elif escolha != '1':
+            execucao = False
+            audio.limpar_tela()
+
+        
+main()
