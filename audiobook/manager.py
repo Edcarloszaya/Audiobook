5a9b3ec4866c9b33fcd0b3e25550c45c46971130
Edcarloszaya <edcarlos.molequedoido@gmail.com>
2024-08-30 10:17:50 -0300

    Na classe AudioBook :
    Foi criado duas funcao:
    
    barra_progresso: cria uma barra de progresso extraindo o texto.
    
    limpar_tela: Limpa o terminal cada escolha.

--- audiobook/manager.py
+++ audiobook/manager.py
@@ -2,6 +2,9 @@ import tkinter as tk
 from tkinter import filedialog
 from gtts import gTTS
 import fitz
+import sys
+import os
+
 
 
 class AudioBook:
@@ -10,7 +13,7 @@ class AudioBook:
         pass
 
     def selecionar_arquivo(self):
-
+        print('\nSelecionar seu PDF!!!')
         janela = tk.Tk()
         janela.withdraw()
 
@@ -20,19 +23,40 @@ class AudioBook:
         return caminho_do_arquivo
 
     def extrair_texto(self, caminho_do_PDF):
+        print("\nextraindo texto !....")
         documento = fitz.open(caminho_do_PDF)
         texto = ""
 
-        for page_num in range(len(documento)):
-            pagina = documento.load_page(page_num)
+        total_paginas = documento.page_count
+
+        for i in range(total_paginas):
+            pagina = documento.load_page(i)
             texto += pagina.get_text()
+            perc = (i + 1) * 100 / total_paginas
+            self.barra_progresso(perc)
+            print("\nTexto extraído com sucesso!")
 
         return texto
 
     def comverti_texto(self, texto):
+        print(f"\nComvertendo seu pdf em audio!\nTempo estimado pelo tamanho do pdf!....\nPfv espere termina...!!!")
         if texto:
             tts = gTTS(text=texto, lang="en", slow=False)
             tts.save("audiobook.mp3")
-            print("Audiobook criado!")
+            print("\nAudiobook criado!")
         else:
             print("Audiobook nao foi criado!")
+
+    def barra_progresso(self,perc):
+        largura = 50
+        bloco = int(round(largura * perc / 100))
+        progresso = "#" * bloco + "-" * (largura - bloco)
+        sys.stdout.write(f"\r[{progresso}] {perc}%")
+        sys.stdout.flush()
+
+    def limpar_tela(self):
+        # Verifica o sistema operacional
+        if os.name == 'nt':  # Windows
+            os.system('cls')
+        else:  # Unix/Linux/MacOS
+            os.system('clear')
